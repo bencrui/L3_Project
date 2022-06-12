8daab6bf4d723bfe08dd2a492fc2713a8e1606e8
bnc4642 <bnc464464@gmail.com>
2022-06-12 18:00:11 +1200

    Finished Basic Animation
    The basic scroll is done, although there will be changes to increase the polish

--- L3CompSci/Assets/Scripts/Feed.cs
+++ L3CompSci/Assets/Scripts/Feed.cs
@@ -8,15 +8,20 @@ public class Feed : MonoBehaviour
     public GameObject Story;
     public GameObject Story2;
     public GameObject Story3;
+    public GameObject StoryP;
 
     public List<GameObject> sL = new List<GameObject>();
 
     void Start()
     {
+        NewStory(true);
         sL.Add(Story);
         sL.Add(Story2);
         sL.Add(Story3);
 
+        sL[0].GetComponent<Animator>().SetBool("s0>1", true);
+        sL[1].GetComponent<Animator>().SetBool("s0>1", true);
+        sL[2].GetComponent<Animator>().SetBool("s0>1", true);
         sL[1].GetComponent<Animator>().SetBool("s1>2", true);
         sL[2].GetComponent<Animator>().SetBool("s1>2", true);
         sL[2].GetComponent<Animator>().SetBool("s2>3", true);
@@ -32,7 +37,39 @@ public class Feed : MonoBehaviour
 
     void Scroll()
     {
-        sL[0].GetComponent<Animator>().SetBool("s1>2", true);
-        sL[1].GetComponent<Animator>().SetBool("s2>3", true);
+        for (int i = 0; i < 4; i++)
+        {
+            sL[i].GetComponent<Animator>().SetBool("s"+(i)+">"+(i+1), true);
+        }
+
+        // need to change to when animation is finished:
+        GameObject.Destroy(sL[3]);
+
+        // move within array
+        for (int i = 3; i > 0; i--)
+        {
+            sL[i] = sL[i - 1];
+        }
+
+        NewStory(false);
+    }
+
+    void NewStory(bool starting)
+    {
+        int index = 0;
+
+        if (starting)
+        {
+            index = sL.Count;
+            sL.Add(Instantiate(StoryP, GetComponentInParent<Transform>()));
+        }
+        else
+        {
+            index = 0;
+            sL[0] = Instantiate(StoryP, GetComponentInParent<Transform>());
+        }
+
+        sL[index].transform.localScale = new Vector3(240, 280, 0);
+        sL[index].transform.position = new Vector3(6.4f, 12.25f, 0);
     }
 }
